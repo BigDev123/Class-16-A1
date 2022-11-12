af3620f98394470b9aad10939d61540ad779d027
BigDev123 <111181248+BigDev123@users.noreply.github.com>
2022-11-12 17:57:27 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var box1;
+
+function setup() {
+  createCanvas(600, 400);
+  box1=new Box()
+  box2=new Box()
+  box3=new Box()
+}
+
+function draw() {
+  background(220);
+box1.show()
+box1.move(2)
+box2.show()
+box2.move(3)
+box3.show()
+box3.move(4)
+}
+
